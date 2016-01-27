d85e078284dfcfa5a7b95bb9a6188c8c0f06b409
Andrea Halweg-Edwards <andrea.edwards@colorado.edu>
2016-01-26 17:01:32 -0700

    add BSD license

--- src/DYNAMCC_0.py
+++ src/DYNAMCC_0.py
@@ -1,5 +1,32 @@
-# for the correct perl version use:: /perl5/perlbrew/perls/perl-5.22.0/bin/perl
+"""
+Copyright (c) 2016, Andrea Halweg-Edwards, Gur Pines, Assaf Pines, James Winkler
+All rights reserved.
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of DYNAMCC nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 
 # !/opt/local/bin/python2.7
 import multiprocessing
@@ -173,40 +200,6 @@ def RemoveCodonByRank(rank, sorted_dict):
 
     return new_dict
 
-def ByUsage(filtered_dict, max_threshold):
-    """This script is called when the user decides to remove codons by usage
-    frequency. To determine the minimum usage frequency that can be accepted
-    as input without inadvertently omitting codons, the script calls 
-    FindMinimumThreshold. Then, the script prompts the user for a cutoff 
-    threshold. If the entry passes error checking, the threshold is passed
-    to RemoveLowCodons.
-
-    Parameters
-    ----------
-    filtered_dict : dict
-        Dictionary formatted in the same way as EditUsageDict(). Dictionary of 
-        lists of dictionaries for codon usage. Technically, any 
-        dictionary that has single letter amino acid symbols as keys would
-        work
-
-    Returns
-    -------
-    new_dict : dict
-        The script accepts user input, checks for an acceptable entry (must
-        be below a stated max as determined in FindMinimumThreshold), and 
-        funnels the entry to RemoveLowCodons, and returns the new_dict from 
-        RemoveLowCodons
-
-    Examples
-    --------
-    >>> selection = RemoveCodonBy()
-    >>> if selection == 'R':
-    >>>     ByRank(filtered_dict)
-    >>> else:
-    >>>     ByUsage(filtered_dict)
-    """
-    return RemoveLowCodons(threshold, filtered_dict)
-
 def SetRedundancy():
     """
     Parameters
@@ -332,3 +325,5 @@ def main():
 
 if(__name__ == '__main__'):
     main()
+
+# for the correct perl version use:: /perl5/perlbrew/perls/perl-5.22.0/bin/perl
