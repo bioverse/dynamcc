f7c7f6c9f1b262318b9b685a077a1fea05834ae7
rahilwazir <rahilwazirali@gmail.com>
2019-10-24 19:33:32 +0500

    Processing non standard amino acids

--- handlers/dynamcc_handlers.py
+++ handlers/dynamcc_handlers.py
@@ -356,7 +356,7 @@ class DynamccDHandler(RequestHandler):
 			non_standard_aas = list(set(usage_table.keys()).difference(aa))
 			non_standard_usage_table = defaultdict(list)
 			if non_standard_aas:
-				non_standard_usage_table = { non_standard_aa: usage_table[non_standard_aa] for non_standard_aa in non_standard_aas }
+				non_standard_usage_table = { non_standard_aa: new_usage_table[non_standard_aa] for non_standard_aa in non_standard_aas }
 
 			return self.render("dynamcc_d.html", error=None, target_codon_aa=target_codon_aa, amino_acids=amino_acids, step=form_step, target_codon=target_codon, hamming_distance=hamming_distance_label, organism_name=organism_name, usage_table=new_usage_table, ns_usage_table=non_standard_usage_table)
 
